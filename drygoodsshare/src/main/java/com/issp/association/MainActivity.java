ae236dd436bba545a959e52f0baa31e107a09806
huanghuanlai <102535481@qq.com>
2017-03-16 19:16:14 +0800

    干货分享

--- drygoodsshare/src/main/java/com/issp/association/MainActivity.java
+++ drygoodsshare/src/main/java/com/issp/association/MainActivity.java
@@ -1,17 +1,20 @@
 package com.issp.association;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import com.issp.association.adapter.BannerImageLoader;
 import com.issp.association.base.view.BaseMvpActivity;
 import com.issp.association.bean.ShareBean;
 import com.issp.association.interfaces.IShareListView;
 import com.issp.association.presenters.ShareInfoPresenter;
 import com.issp.association.utils.DisplayUtils;
+import com.youth.banner.Banner;
 
 import java.util.ArrayList;
 
@@ -32,12 +35,43 @@ public class MainActivity extends BaseMvpActivity<IShareListView, ShareInfoPrese
     TextView lt_main_title;
     @BindView(R.id.lt_main_title_right)
     TextView lt_main_title_right;
+    @BindView(R.id.homepage_lv)
+    RecyclerView homepage_lv;
+
+    View view_share_page_header;
+    Banner homepage_banner;
+    private ArrayList<String> imgList;
+    String[] images={"https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=699105693,866957547&fm=21&gp=0.jpg",
+            "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=787324823,4149955059&fm=21&gp=0.jpg",
+            "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2152422253,1846971893&fm=21&gp=0.jpg",
+            "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3258213409,1470632782&fm=21&gp=0.jpg"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initView();
+    }
+    private void initView(){
+        view_share_page_header = View.inflate(MainActivity.this, R.layout.view_share_page_header, null);
+        homepage_banner= (Banner) view_share_page_header.findViewById(R.id.homepage_banner);
+        initBanner();
+
+
+    }
+
+    private void initBanner() {
+        imgList=new ArrayList<>();
+        for (int i = 0; i < images.length; i++) {
+            imgList.add(images[i]);
+        }
+        //设置图片加载器
+        homepage_banner.setImageLoader(new BannerImageLoader());
+        //设置图片集合
+        homepage_banner.setImages(imgList);
+        //banner设置方法全部调用完毕时最后调用
+        homepage_banner.start();
     }
 
     @Override
